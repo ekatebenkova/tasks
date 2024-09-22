ad2f1b8310a0de76e97ffb7617c1d4fd0e7b7fa5
Ekaterina Tebenkova <biruta78@gmail.com>
2024-09-22 21:22:37 +0500

    design base board

--- src/App.js
+++ src/App.js
@@ -1,8 +1,95 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Задачи</header>
+          <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+          </ul>
+          <footer>Добавить новую задачу</footer>
+        </div>
 
+        <div className="column">
+          <header>В работе</header>
+          <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+          </ul>
+          <footer>Добавить новую задачу</footer>
+        </div>
+
+        <div className="column">
+          <header>Выполнено</header>
+          <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+          </ul>
+          <footer>Добавить новую задачу</footer>
+        </div>
+
+        <div className="column">
+          <header>Проверено</header>
+          <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+          </ul>
+          <footer>Добавить новую задачу</footer>
+        </div>
+
+        <div className="column">
+          <header>Проверено</header>
+          <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+          </ul>
+          <footer>Добавить новую задачу</footer>
+        </div>
+
+        <div className="column">
+          <header>Проверено</header>
+          <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>first</li>
+            <li>second</li>
+          </ul>
+          <footer>Добавить новую задачу</footer>
+        </div>
+
+      </div>
     </div>
   );
 }
